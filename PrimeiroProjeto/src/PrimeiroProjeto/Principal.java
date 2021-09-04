3877f8df468b9010ba750470bc3dad88b36874ae
isaquejunior <44180952+isaquejunior@users.noreply.github.com>
2021-09-04 10:44:16 -0300

    Add files via upload

---
+++ PrimeiroProjeto/src/PrimeiroProjeto/Principal.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package PrimeiroProjeto;
+
+import java.util.Scanner;
+
+/**
+ * @author isaque.santos
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Olá amigo!\nQual é o seu nome?"); //Uso do println
+		
+		//Ler uma string do console usando a bliblioteca Scanner.
+		Scanner in  = new Scanner(System.in);
+		String nome = in.nextLine();
+		
+		//Exibir uma string usando printf
+		System.out.printf("Olá %s!\n", nome);
+
+	}
+
+}
